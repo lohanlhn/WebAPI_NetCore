82b92fb8740129c0394ebcc064cab7a0f135ac13
lohanlhn <lohan.massote@gmail.com>
2020-09-11 11:59:50 -0300

    Controller iniciado

---
+++ WebAPI_NetCore/Controllers/ProdutosController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebAPI_NetCore.Context;
+using WebAPI_NetCore.Models;
+
+namespace WebAPI_NetCore.Controllers
+{
+    [Route("api/[Controller]")]
+    [ApiController]
+    public class ProdutosController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+        public ProdutosController(AppDbContext contexto)
+        {
+            _context = contexto;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Produto>> Get()
+        {
+            return _context.Produtos.AsNoTracking().ToList();
+        }
+
+        [HttpGet("{id}", Name = "ObterProduto")]
+        public ActionResult<Produto> Get(int id)
+        {
+            var produto = _context.Produtos.AsNoTracking().FirstOrDefault(p => p.ProdutoId == id);
+
+            if(produto == null)
+            {
+                return NotFound();
+            }
+            return produto;
+        }
+
+        [HttpPost]
+        public ActionResult Post([FromBody]Produto produto)
+        {
+            _context.Produtos.Add(produto);
+            _context.SaveChanges();
+            return new CreatedAtRouteResult("ObterProduto", new { id = produto.ProdutoId }, produto);
+        }
+    }
+}
---
+++ WebAPI_NetCore/Migrations/20200911130408_Populadb.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WebAPI_NetCore.Migrations
+{
+    public partial class Populadb : Migration
+    {
+        protected override void Up(MigrationBuilder mb)
+        {
+            mb.Sql("insert into Categorias(Nome,ImageUrl) values('Bebidas', 'https://saofranciscodoconde.ba.gov.br/wp-content/uploads/2017/04/agua11.jpg')");
+            mb.Sql("insert into Categorias(Nome,ImageUrl) values('Lanches', 'https://nerilanches.com.br/dominios/nerilanches.com.br/img/slider/slide1.png')");
+            mb.Sql("insert into Categorias(Nome,ImageUrl) values('Sobremesas', 'https://www.sabornamesa.com.br/media/k2/items/cache/dd45d054dfce696b68bc0b43a11d1bfe_L.jpg')");
+
+            mb.Sql("insert into Produtos(Nome, Descricao, Preco, ImageUrl, Estoque, DataCadastro, CategoriaId)" +
+                    "values('Lanche de Atum', 'Lanche de Atum com maionese', 8.50, " +
+                    "'https://img.itdg.com.br/tdg/images/recipes/000/003/165/38827/38827_original.jpg',10, GETDATE(), (select CategoriaId from Categorias where Nome= 'Lanches'))");
+
+            mb.Sql("insert into Produtos(Nome, Descricao, Preco, ImageUrl, Estoque, DataCadastro, CategoriaId)" +
+                    "values('Pudim 100g', 'Pudim de leite condensado', 6.75, " +
+                    "'https://img.itdg.com.br/tdg/images/recipes/000/031/593/318825/318825_original.jpg',20, GETDATE(), (select CategoriaId from Categorias where Nome= 'Sobremesas'))");
+
+            mb.Sql("insert into Produtos(Nome, Descricao, Preco, ImageUrl, Estoque, DataCadastro, CategoriaId)" +
+                    "values('Coca-Cola Diet', 'Refrigerante de Cola 350ml', 5.45, " +
+                    "'https://images-na.ssl-images-amazon.com/images/I/41NZjtqw9nL.jpg',50, GETDATE(), (select CategoriaId from Categorias where Nome= 'Bebidas'))");
+        }
+
+        protected override void Down(MigrationBuilder mb)
+        {
+            mb.Sql("Delete from Categorias");
+            mb.Sql("Delete from Produtos");
+        }
+    }
+}
