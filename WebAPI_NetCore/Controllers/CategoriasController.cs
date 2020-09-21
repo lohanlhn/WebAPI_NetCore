ddf3110798ce178350eacc6024525e0c61db7ddc
lohanlhn <lohan.massote@gmail.com>
2020-09-21 12:03:06 -0300

    CategoriasController criado

---
+++ WebAPI_NetCore/Controllers/CategoriasController.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebAPI_NetCore.Context;
+using WebAPI_NetCore.Models;
+
+namespace WebAPI_NetCore.Controllers
+{
+    [Route("api/[Controller]")]
+    [ApiController]
+    public class CategoriasController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public CategoriasController(AppDbContext contexto)
+        {
+            _context = contexto;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Categoria>> Get()
+        {
+            //AsNoTracking para desabilitar o gerenciamento de estado dos objetos (alterações)
+            //Deixando o codigo com mais performance
+            return _context.Categorias.AsNoTracking().ToList();
+        }
+
+        [HttpGet("{id}", Name = "ObterCategoria")]
+        public ActionResult<Categoria> Get(int id)
+        {
+            var categoria = _context.Categorias.AsNoTracking().FirstOrDefault(c => c.CategoriaId == id);
+
+            if(categoria == null)
+            {
+                return NotFound();
+            }
+            return categoria;
+        }
+        [HttpPost]
+        public ActionResult Post([FromBody] Categoria categoria)
+        {
+            _context.Categorias.Add(categoria);
+            _context.SaveChanges();
+            return new CreatedAtRouteResult("ObterCategoria", new { id = categoria.CategoriaId }, categoria);
+        }
+
+        [HttpPut("{id}")]
+        public ActionResult Put(int id, [FromBody] Categoria categoria)
+        {
+            if (categoria.CategoriaId != id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(categoria).State = EntityState.Modified;
+            _context.SaveChanges();
+            return Ok();
+        }
+    }
+}
